9f27118b9b0b0730029507473ebf938e21957b16
carson mulligan <carsonmulligan45@gmail.com>
2022-05-25 15:35:40 +0100

    minor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Chef.delete_all
 10.times do
   user_chef = User.create(first_name: Faker::Name::female_first_name, last_name: Faker::Name::last_name, password: 'secret', email: Faker::Internet.email, chef_boolean: true)
 
-  chef = Chef.new(nickname: Faker::Superhero.name, cuisine: Faker::Food.ethnic_category, description: Faker::GreekPhilosophers.quote, location: Faker::Nation.flag, price: Faker::Number.number(digits: 3))
+  chef = Chef.new(nickname: Faker::Name.name, cuisine: Faker::Food.ethnic_category, description: Faker::GreekPhilosophers.quote, location: Faker::Nation.capital_city, price: Faker::Number.number(digits: 3))
   chef.user = user_chef
   chef.save
 end
