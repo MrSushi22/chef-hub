fcb820d9ec30afd6bfea20962292c8a685b3b6f4
carson mulligan <carsonmulligan45@gmail.com>
2022-05-25 16:36:38 +0100

    added index to bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,8 @@
 class BookingsController < ApplicationController
 
-
+  def index
+    @bookings = Booking.all
+  end
   def new
     @booking = Booking.new
     @chef = Chef.find(params[:chef_id])
@@ -11,13 +13,17 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.chef = Chef.find(params[:chef_id])
     if @booking.save
-      redirect_to chefs_path
+      redirect_to chef_bookings_path
     else
       render :new
     end
   end
 
   def destroy
+  end
+
+  def show
+    @chef = Chef.find(params[:chef_id])
   end
 
   private
