8102ef8fd159f53bb8bd78213345472c4369a8a0
carson mulligan <carsonmulligan45@gmail.com>
2022-05-26 15:58:43 +0100

    testing avatar functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,5 @@ end
 private
 
 def user_params
-  params.require(:user).permit(:first_name, :last_name, :email, :password, :photo)
+  params.require(:user).permit(:first_name, :last_name, :email, :password, :avatar)
 end
