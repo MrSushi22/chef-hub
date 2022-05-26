42ec4f0e97a4673f1d8e1f361f5775f0d3f848a1
carson mulligan <carsonmulligan45@gmail.com>
2022-05-26 16:41:06 +0100

    added geocoded_by :address to Chef Model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -7,4 +7,8 @@ class Chef < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   validates :location, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if:
+:will_save_change_to_address?
 end
